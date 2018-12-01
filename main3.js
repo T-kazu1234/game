7d24cfcae54ee46e8b3342e5babcb3797941fb0e
T-kazu1234 <tarchan00@gmail.com>
2018-12-02 06:40:28 +0900

    Update main3.js
    サンプル画像を保存したので、フォルダ階層の指定を追加した

--- main3.js
+++ main3.js
@@ -8,10 +8,10 @@
  
  window.onload = function(){
  	var core = new Core(320,320);
- 	core.preload('chara1.png');
+ 	core.preload('image/sample/chara1.png');
   	core.onload = function() {
  	var bear = new Sprite(32,32);
- 	bear.image = core.assets['chara1.png']
+ 	bear.image = core.assets['image/sample/chara1.png']
  	bear.x = 0;
  	bear.y = 0;
  	core.rootScene.addChiled(bear);
