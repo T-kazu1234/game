34b99995f523d6b3a4fa30dbaaf84714350fe333
jGXdxDb6Bio <44921951+jGXdxDb6Bio@users.noreply.github.com>
2018-12-03 18:11:49 +0900

    Update main4.js

--- main4.js
+++ main4.js
@@ -1,7 +1,7 @@
 enchant();
 
 window.onload = function(){
-    var core = new Core(1080,1920);
+    var core = new Core(700,1400);
     core.preload('tail.png','back_hair.png','right_leg.png','left_leg.png');
     core.preload('right_hand.png','left_hand.png','body.png','face.png');
     core.preload('neck.png','right_wing.png','left_wing.png','hair.png');
@@ -13,113 +13,115 @@ window.onload = function(){
         
         var toki = new Sprite(700,1400);
         toki.image = core.assets['z.png'];
-        toki.x = 300;
+        toki.x = 0;
         toki.y = 0;
         core.rootScene.addChild(toki);
         
         //尻尾の初期設定
         var tail = new Sprite(350,250);
         tail.image = core.assets['tail.png'];
-        tail.x = 0;
-        tail.y = 0;
+        tail.x = 556-339;
+        tail.y = 699;
         core.rootScene.addChild(tail);
         
         //後ろ髪の初期設定
         var back_hair = new Sprite(400,300);
         back_hair.image = core.assets['back_hair.png'];
-        back_hair.x = 0;
-        back_hair.y = 0;
+        back_hair.x = 169-12;
+        back_hair.y = 539-245;
         core.rootScene.addChild(back_hair);
         
         //右脚の初期設定
         var right_leg = new Sprite(200,926);
         right_leg.image = core.assets['right_leg.png'];
-        right_leg.x = 0;
-        right_leg.y = 0;
+        right_leg.x = 208-4;
+        right_leg.y = 1309-926;
+        right_leg.rotation = 30;
         core.rootScene.addChild(right_leg);
         
         //左脚の初期設定
         var left_leg = new Sprite(200,918);
         left_leg.image = core.assets['left_leg.png'];
-        left_leg.x = 0;
-        left_leg.y = 0;
+        left_leg.x = 399-93;
+        left_leg.y = 1309-918;
+        left_leg.rotation = -30;
         core.rootScene.addChild(left_leg);
         
         //右腕の初期設定
-        var right_hand = new Sprite(190,852);
+        var right_hand = new Sprite(300,740);
         right_hand.image = core.assets['right_hand.png'];
-        right_hand.x = 0;
-        right_hand.y = 0;
+        right_hand.x = 137;
+        right_hand.y = 956-740;
         core.rootScene.addChild(right_hand);
         
         //左腕の初期設定
-        var left_hand = new Sprite(190,852);
+        var left_hand = new Sprite(300,740);
         left_hand.image = core.assets['left_hand.png'];
-        left_hand.x = 0;
-        left_hand.y = 0;
+        left_hand.x = 137+74+52;
+        left_hand.y = 958-740;
         core.rootScene.addChild(left_hand);
         
         //身体の初期設定
         var body = new Sprite(400,400);
         body.image = core.assets['body.png'];
-        body.x = 0;
-        body.y = 0;
+        body.x = 538-382;
+        body.y = 915-390;
         core.rootScene.addChild(body);
         
         //眼の初期設定
         var eye = new Sprite(130,60);
         eye.image = core.assets['eye.png'];
-        eye.x = 0;
-        eye.y = 0;
+        eye.x = 325-40;
+        eye.y = 480-56;
         core.rootScene.addChild(eye);
         
         //顔の初期設定
         var face = new Sprite(225,225);
         face.image = core.assets['face.png'];
-        face.x = 0;
-        face.y = 0;
+        face.x = 442-205;
+        face.y = 580-222;
         core.rootScene.addChild(face);
         
         //首の初期設定
         var neck = new Sprite(250,150);
         neck.image = core.assets['neck.png'];
-        neck.x = 0;
-        neck.y = 0;
+        neck.x = 237-17;
+        neck.y = 606-106;
         core.rootScene.addChild(neck);
         
         //右羽の初期設定
         var right_wing = new Sprite(360,360);
         right_wing.image = core.assets['right_wing.png'];
-        right_wing.x = 0;
-        right_wing.y = 0;
+        right_wing.x = 172-58;
+        right_wing.y = 121-7;
         core.rootScene.addChild(right_wing);
         
         //左羽の初期設定
         var left_wing = new Sprite(360,360);
         left_wing.image = core.assets['left_wing.png'];
-        left_wing.x = 0;
-        left_wing.y = 0;
+        left_wing.x = 414-180;
+        left_wing.y = 125-5;
         core.rootScene.addChild(left_wing);
         
         //髪の初期設定
         var hair = new Sprite(400,500);
         hair.image = core.assets['hair.png'];
-        hair.x = 0;
-        hair.y = 0;
+        hair.x = 187-37;
+        hair.y = 745-488;
         core.rootScene.addChild(hair);
         
         function idou(name){
             if (core.input.down){
-                name.y+=5;
+                name.y+=1;
             };
             if (core.input.up){
-                name.y-=5;
+                name.y-=1;
             };
             if (core.input.left){
-                name.x-=5;
+                name.x-=1;
             };
             if (core.input.right){
-                name.x+=5;
+                name.x+=1;
             };
         };
         
@@ -138,86 +140,29 @@ window.onload = function(){
             };
         };
         
+        function lll(name){
+            if (core.input.down){
+                name.rotation-=1;
+            };
+            if (core.input.up){
+                name.rotation+=1;
+            };
+            if (core.input.left){
+                name.rotation+=1;
+            };
+            if (core.input.right){
+                name.rotation-=1;
+            };
+        };
+        
         tail.addEventListener('enterframe',function(){
-                              if((eee%27)==1){
-                              idou(tail);
-                              };
-                              if((eee%27)==2){
-                              rrr(tail);
-                              };
-                              if((eee%27)==3){
-                              idou(back_hair);
-                              };
-                              if((eee%27)==4){
-                              rrr(back_hair);
-                              };
-                              if((eee%27)==5){
-                              idou(right_leg);
-                              };
-                              if((eee%27)==6){
                               rrr(right_leg);
-                              };
-                              if((eee%27)==7){
-                              idou(left_leg);
-                              };
-                              if((eee%27)==8){
-                              rrr(left_leg);
-                              };
-                              if((eee%27)==9){
-                              idou(right_hand);
-                              };
-                              if((eee%27)==10){
+                              lll(left_leg);
                               rrr(right_hand);
-                              };
-                              if((eee%27)==11){
-                              idou(left_hand);
-                              };
-                              if((eee%27)==12){
-                              rrr(left_hand);
-                              };
-                              if((eee%27)==13){
-                              idou(body);
-                              };
-                              if((eee%27)==14){
-                              rrr(body);
-                              };
-                              if((eee%27)==15){
-                              idou(eye);
-                              };
-                              if((eee%27)==16){
-                              rrr(eye);
-                              };
-                              if((eee%27)==17){
-                              idou(face);
-                              };
-                              if((eee%27)==18){
-                              rrr(face);
-                              };
-                              if((eee%27)==19){
-                              idou(neck);
-                              };
-                              if((eee%27)==20){
-                              rrr(neck);
-                              };
-                              if((eee%27)==21){
-                              idou(right_wing);
-                              };
-                              if((eee%27)==22){
+                              lll(left_hand);
                               rrr(right_wing);
-                              };
-                              if((eee%27)==23){
-                              idou(left_wing);
-                              };
-                              if((eee%27)==24){
-                              rrr(left_wing);
-                              };
-                              if((eee%27)==25){
-                              idou(hair);
-                              };
-                              if((eee%27)==26){
-                              rrr(hair);
-                              };
-                              if(eee==26){toki.x=1000};
+                              lll(left_wing);
+                              idou(eye);
                               });
         
         core.rootScene.on('touchstart',function(e){
