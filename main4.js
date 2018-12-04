7ffbead58030edc36eb1e34892ac8c34757941d8
jGXdxDb6Bio <44921951+jGXdxDb6Bio@users.noreply.github.com>
2018-12-05 05:24:33 +0900

    Update main4.js

--- main4.js
+++ main4.js
@@ -5,7 +5,7 @@ window.onload = function(){
     core.preload('tail.png','back_hair.png','right_leg.png','left_leg.png');
     core.preload('right_hand.png','left_hand.png','body.png','face.png');
     core.preload('neck.png','right_wing.png','left_wing.png','hair.png');
-    core.preload('eye.png','eyes.png');
+    core.preload('eye.png','eyes.png','z.png');
     core.fps = 100;
     core.onload = function(){
         
@@ -14,99 +14,106 @@ window.onload = function(){
         var iii=1;
         var face1 = 0;
         
+        //全体の初期設定
+        var z = new Sprite(700,1400);
+        z.image = core.assets['z.png'];
+        z.x = 0;
+        z.y = 0;
+        core.rootScene.addChild(z);
+        
         //尻尾の初期設定
         var tail = new Sprite(350,250);
         tail.image = core.assets['tail.png'];
-        tail.x = 556-339;
+        tail.x = 217+300;
         tail.y = 699;
         core.rootScene.addChild(tail);
         
         //後ろ髪の初期設定
         var back_hair = new Sprite(400,300);
         back_hair.image = core.assets['back_hair.png'];
-        back_hair.x = 169-12;
-        back_hair.y = 539-245;
+        back_hair.x = tail.x-60;
+        back_hair.y = tail.y-405;
         core.rootScene.addChild(back_hair);
         
         //右脚の初期設定
         var right_leg = new Sprite(200,926);
         right_leg.image = core.assets['right_leg.png'];
-        right_leg.x = 208-4;
-        right_leg.y = 1309-926;
+        right_leg.x = tail.x-13;
+        right_leg.y = tail.y-316;
         core.rootScene.addChild(right_leg);
         
         //左脚の初期設定
         var left_leg = new Sprite(200,918);
         left_leg.image = core.assets['left_leg.png'];
-        left_leg.x = 399-93;
-        left_leg.y = 1309-918;
+        left_leg.x = tail.x+89;
+        left_leg.y = tail.y-308;
         core.rootScene.addChild(left_leg);
         
         //右腕の初期設定
         var right_hand = new Sprite(300,740);
         right_hand.image = core.assets['right_hand.png'];
-        right_hand.x = 137;
-        right_hand.y = 956-740;
+        right_hand.x = tail.x-80;
+        right_hand.y = tail.y-483;
         core.rootScene.addChild(right_hand);
         
         //左腕の初期設定
         var left_hand = new Sprite(300,740);
         left_hand.image = core.assets['left_hand.png'];
-        left_hand.x = 137+74+56;
-        left_hand.y = 956-740;
+        left_hand.x = tail.x+50;
+        left_hand.y = tail.y-483;
         core.rootScene.addChild(left_hand);
         
         //身体の初期設定
         var body = new Sprite(400,400);
         body.image = core.assets['body.png'];
-        body.x = 538-382;
-        body.y = 915-390;
+        body.x = tail.x-61;
+        body.y = tail.y-174;
         core.rootScene.addChild(body);
         
         //眼の初期設定
         var eye = new Sprite(130,60);
         eye.image = core.assets['eye.png'];
-        eye.x = 325-40;
-        eye.y = 480-56;
+        eye.x = tail.x+68;
+        eye.y = tail.y-275;
         core.rootScene.addChild(eye);
         
         //顔の初期設定
         var face = new Sprite(225,225);
         face.image = core.assets['face.png'];
-        face.x = 442-205;
-        face.y = 580-222;
+        face.x = tail.x+20
+        face.y = tail.y-341;
         face.frame = 3;
         core.rootScene.addChild(face);
         
         //首の初期設定
         var neck = new Sprite(250,150);
         neck.image = core.assets['neck.png'];
-        neck.x = 237-17;
-        neck.y = 606-106;
+        neck.x = tail.x+3;
+        neck.y = tail.y-199;
         core.rootScene.addChild(neck);
         
         //右羽の初期設定
         var right_wing = new Sprite(360,360);
         right_wing.image = core.assets['right_wing.png'];
-        right_wing.x = 172-58;
-        right_wing.y = 121-7;
+        right_wing.x = tail.x-103
+        right_wing.y = tail.y-585;
         core.rootScene.addChild(right_wing);
         
         //左羽の初期設定
         var left_wing = new Sprite(360,360);
         left_wing.image = core.assets['left_wing.png'];
-        left_wing.x = 414-180;
-        left_wing.y = 125-5;
+        left_wing.x = tail.x+17;
+        left_wing.y = tail.y-579;
         core.rootScene.addChild(left_wing);
         
         //髪の初期設定
         var hair = new Sprite(400,500);
         hair.image = core.assets['hair.png'];
-        hair.x = 187-37;
-        hair.y = 745-488;
+        hair.x = tail.x-67;
+        hair.y = tail.y-442;
         core.rootScene.addChild(hair);
         
-        //髪の初期設定
+        //バックベアードの初期設定
         var eyes = new Sprite(173,129);
         eyes.image = core.assets['eyes.png'];
         eyes.x = 280;
