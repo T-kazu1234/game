8ea15eea41a40292b6f4f9ccc06c2390b09b4f64
T-kazu1234 <tarchan00@gmail.com>
2019-01-08 21:56:33 +0900

    youtube再生クラスの作成
    /*
     Entityクラスの使い方
    
    1:enchant.js - Entity の背景色を変えてみよう(rgb)
      http://jsdo.it/phi/9KjM
    
    2:【enchant.js】Entity()メソッドまとめ。
      http://note.onichannn.net/archives/1571
      
    3: 公式リファレンス(Class enchant.Entity)
      http://wise9.github.io/enchant.js/doc/core/ja/symbols/enchant.Entity.html#visible
    */
    
    //////////////////////////////////////////////////////////////////////////
    /*iframeタグを埋め込みます。
      https://www.youtube.com/embed/HNYkOJ-T63k?enablejsapi=1..のHNYkOJ-T63kが動画IDです。
      ここを変更することで動画を変更できます。
      その他詳細はhttps://developers.google.com/youtube/js_api_reference?hl=jaを参照してください。*/
    //////////////////////////////////////////////////////////////////////////

--- GameFolder04/game.js
+++ GameFolder04/game.js
@@ -399,7 +399,20 @@ window.onload = function() {
       textLayer = new Group();
       core.rootScene.addChild(textLayer);
 
-
+//////////////////////////////////////////////////////////////////////////
+//  youtube再生
+//////////////////////////////////////////////////////////////////////////
+      YOUTUBE_ID = '3uRdxB6GdCc';
+      video = new Entity()
+      video.visible =  true;
+      video.opacity =  0.1;
+      video._element = document.createElement('div')
+      video.x = 10 //width  //動画設X置座標
+      video.y = 10 //height //動画設置Y座標
+      video._element.innerHTML = '<iframe src="https://www.youtube.com/embed/'+YOUTUBE_ID+'?enablejsapi=1&controls=0&showinfo=0&autoplay=0&rel=0&vq=small"  width="10" height="10" frameborder="0" id="player"></iframe>'
+      core.rootScene.addChild(video); // # 動画をシーンに追加
+//////////////////////////////////////////////////////////////////////////
+      
       /*これが最初のシーンです */
       _currentScene.SetBackGroundImage("title.png");
       //引数リスト
