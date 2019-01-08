a10d9a2e72c322b8b2a36cb8480d39d038792819
T-kazu1234 <tarchan00@gmail.com>
2019-01-08 23:49:11 +0900

    透明度とyoutubeプレイヤーサイズの変更

--- GameFolder04/game.js
+++ GameFolder04/game.js
@@ -405,11 +405,11 @@ window.onload = function() {
       YOUTUBE_ID = '3uRdxB6GdCc';
       video = new Entity()
       video.visible =  true;
-      video.opacity =  0.1;
+      video.opacity =  1;
       video._element = document.createElement('div')
       video.x = 10 //width  //動画設X置座標
       video.y = 10 //height //動画設置Y座標
-      video._element.innerHTML = '<iframe src="https://www.youtube.com/embed/'+YOUTUBE_ID+'?enablejsapi=1&controls=0&showinfo=0&autoplay=0&rel=0&vq=small"  width="10" height="10" frameborder="0" id="player"></iframe>'
+      video._element.innerHTML = '<iframe src="https://www.youtube.com/embed/'+YOUTUBE_ID+'?enablejsapi=1&controls=0&showinfo=0&autoplay=0&rel=0&vq=small"  width="100" height="100" frameborder="0" id="player"></iframe>'
       core.rootScene.addChild(video); // # 動画をシーンに追加
 //////////////////////////////////////////////////////////////////////////
       
