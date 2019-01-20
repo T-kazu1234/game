612e4b78768d96031940862ed63990e560c36941
KazushigeTanaka <tarchan00@gmail.com>
2019-01-21 04:14:15 +0900

    フラグ利用シーン作成

--- GameFolder_05/MainPlayer.js
+++ GameFolder_05/MainPlayer.js
@@ -4,12 +4,13 @@ class MainPlayer{
 
     //ここのステースを追加して条件フラグなどを複雑化できます
     this.status = {
-      'miyapoint': 0,
-      'nenepoint':0,
-      'みやがスパイの話を聞いた': false,
-      'ねねに会いたい':false,
-      'みやに会いたい':false,
-      'ねねが殺した話を聞いた':false,
+//    'miyapoint': 0,
+//    'nenepoint':0,
+      '下女達の会話を聞く': false,
+//    'みやがスパイの話を聞いた': false,
+//    'ねねに会いたい':false,
+//    'みやに会いたい':false,
+//    'ねねが殺した話を聞いた':false,
     };
 
   }
