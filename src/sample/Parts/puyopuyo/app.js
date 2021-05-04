e11accdf4448d1c5c6cfadbed3486b6ee9db836e
Tkazu1234 <tarchan00@gmail.com>
2021-05-04 23:48:46 +0900

    コメント修正

--- src/sample/Parts/puyopuyo/app.js
+++ src/sample/Parts/puyopuyo/app.js
@@ -7,8 +7,9 @@ let context = null;
 
 enchant();
 
+
 /**
- * gameLoad
+ * gameLoad 
  * @param {*} width 幅
  * @param {*} height 高さ
  * @param Rectangle 【語源】ラテン語「まっすぐな角」の意　をEnchantクラス生成
@@ -31,10 +32,15 @@ enchant();
 
 
 
-    // ゲームのFPS
+    /**
+     * ゲームのFPS
+     */
     game.fps = 10;
 
-    // 必要なファイルを相対パスで引数に指定する。 ファイルはすべて、ゲームが始まる前にロードされる。
+    /**
+     * 必要なファイルを相対パスで引数に指定する。 
+     * ファイルはすべてゲームが始まる前にロードされる。
+     */
     game.preload(
         "images/resize/puyopuyo2_64.png", 
         "images/resize/map0_64.png",
@@ -44,7 +50,9 @@ enchant();
         //"sound/PuyoPuyo.mp3"
         );
 
-    // ロードが完了したら、ゲームの処理を実行していく
+    /** 
+     *ロードが完了したら、ゲームの処理を実行していく
+     */
     game.onload = function () {
         /**  
          * AudioElement を作成
@@ -66,24 +74,24 @@ enchant();
             audio.pause();
             audio.currentTime = 0;
         };
-                    /**
-                     * 時間経過毎にぷよを下に落とすためのタイマー
-                                                
-                    game.addEventListener('enterframe',function(e){
-                        let PuyoDropTimeCounter = 0;
-                        for(var i = 0;  i < 10000;  i++){
-                                PuyoDropTimeCounter = i;
-                                let TimeCounter = new GameController();
-                                if(PuyoDropTimeCounter==9999)
-                                    TimeCounter.isTimerCount = true,
-                                    TimeCounter.TimerCount(),
-                                    PuyoDropTimeCounter=0;
-                                    console.log("PuyoDropTimeCounter=",PuyoDropTimeCounter)
-                                };
-                                
-                            });
-     */
-        //ゲームスコアラベルを生成
+/**
+ * 時間経過毎にぷよを下に落とすためのタイマー
+                            
+game.addEventListener('enterframe',function(e){
+    let PuyoDropTimeCounter = 0;
+    for(var i = 0;  i < 10000;  i++){
+            PuyoDropTimeCounter = i;
+            let TimeCounter = new GameController();
+            if(PuyoDropTimeCounter==9999)
+                TimeCounter.isTimerCount = true,
+                TimeCounter.TimerCount(),
+                PuyoDropTimeCounter=0;
+                console.log("PuyoDropTimeCounter=",PuyoDropTimeCounter)
+            };
+            
+        });
+*/
+//ゲームスコアラベルを生成
         let label = [];
         label[1] = new Label();
         label[1].moveTo(20 , game.height - 220);
@@ -94,70 +102,60 @@ enchant();
         game.rootScene.addChild(label[1]);
 
 
-        /**
-        * バーチャルパッドの作成
-        *  [images/pad.png]
-        */
+/**
+* バーチャルパッドの作成
+* 「方向キー」[images/pad.png]
+*/
         pad = new Pad();
         pad.x = 0;
         pad.y = game.height - 200;
         pad.addEventListener('enterframe', function(e) {               
-        /**
-        * GameControllerClassからインスタンスpadinputを生成
-        *  [images/CommentImage/PadControl.gif]
-        * @param {boolean} game.input.up 上方向キーが押されたか
-        * @param {boolean} isGameInputUp 
-        *  boolean (true:押された/false:押されていない)
-        * 「方向キー」
-        
-        * pad.rotation padの回転角度(0,180,270,90:上,下,左,右)
-        * pad.frame = 5 パッドが押された画像にパッドフレームを変更
-        * 
-        */
-            var padinput = new GameController();
-                // 上下左右が「どれも押されていない」場合、パッドフレーム0セット
-                //console.log("何も押されていない");
-                if(!game.input.up&&!game.input.down&&!game.input.left&&!game.input.right)
-                    pad.frame = 0;
-                /**上が「押されている」場合:
-                * Class(GameController)のInstance(padinput)のMethod(isGameInputUP(引数=ブール値：初期false)に対し、
-                * padのgame.input.upをセット
-                */
-                if (game.input.up)   
-                    pad.frame = 5 , 
-                    pad.rotation = 0,
-                    padinput.isGameInputUp = game.input.up;
-                    //console.log("MusicPlayをコールした"),
-                    MusicPlay();
-                //下
-                if (game.input.down) 
-                    pad.frame = 5 , 
-                    pad.rotation = 180,
-                    padinput.isGameInputDown = game.input.down;
-                    //console.log("MusicStopをコールした"),
-                    MusicStop();
-                //左
-                if (game.input.left) 
-                    pad.frame = 5 , 
-                    pad.rotation = 270,
-                    padinput.isGameInputLeft = game.input.left;
-                //右
-                if (game.input.right)
-                    pad.frame = 5 , 
-                    pad.rotation = 90,
-                    padinput.isGameInputRight = game.input.right;
-                //インスタンス(padinput)のMethod(padinput)を呼ぶ。上手く呼べたらpadinput内の//console.logが反応
-                    padinput.padinput();
-        });
+/**
+* GameControllerClassからインスタンスpadinputを生成
+*  [images/CommentImage/PadControl.gif]
+* @param {boolean} isGameInputUp 上方向キーが押されたか
+*  boolean (true:押された/false:押されていない)
+* @method rotation padの回転角度　(0,180,270,90:上,下,左,右)
+* @param pad.frame = 0 上下左右が「どれも押されていない」
+* @param pad.frame = 5 :パッドが押された画像にパッドフレームを変更)
+
+* Class(GameController)のInstance(padinput)のMethod(isGameInputUP(引数=ブール値：初期false)に対し、
+* padのgame.input.upをセット
+*/
+var padinput = new GameController();
+    if(!game.input.up&&!game.input.down&&!game.input.left&&!game.input.right)
+        pad.frame = 0;
+//上が「押されている」場合:
+    if (game.input.up)  
+    pad.frame = 5 , 
+    pad.rotation = 0,
+    padinput.isGameInputUp = game.input.up;
+//下
+    if (game.input.down) 
+        pad.frame = 5 , 
+        pad.rotation = 180,
+        padinput.isGameInputDown = game.input.down;
+//左
+    if (game.input.left) 
+        pad.frame = 5 , 
+        pad.rotation = 270,
+        padinput.isGameInputLeft = game.input.left;
+//右
+    if (game.input.right)
+        pad.frame = 5 , 
+        pad.rotation = 90,
+        padinput.isGameInputRight = game.input.right;
+//インスタンス(padinput)のMethod(padinput)を呼ぶ。上手く呼べたらpadinput内の//console.logが反応
+        padinput.padinput();
+});
 
 
-       /**
-         * 回転ボタンの作成
-         * [images/Button.png]
-         * @param {Number} button.x 回転ボタンX座標
-         * @param {Number} button.y 回転ボタンy座標
-         * @param {boolean} InputA 回転ボタンが押されたかの判定
-         */
+/**
+ * 回転ボタンの作成
+ * @param {Number} button.x 回転ボタンX座標
+ * @param {Number} button.y 回転ボタンy座標
+ * @param {boolean} InputA 回転ボタンが押されたかの判定
+ */
         var B_Button = new Button("B","B_button",100,100);
             B_Button.x = game.width-B_Button.width;
             B_Button.y = game.height-B_Button.height;
